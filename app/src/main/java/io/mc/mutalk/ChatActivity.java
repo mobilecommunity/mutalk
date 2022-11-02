b475d2a31226b00b805948c57e69ce98c0d88e33
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2022-11-03 01:08:40 +0900

    Added sign-in and chat activity

---
+++ app/src/main/java/io/mc/mutalk/ChatActivity.java
@@ -0,0 +1,14 @@
+package io.mc.mutalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
