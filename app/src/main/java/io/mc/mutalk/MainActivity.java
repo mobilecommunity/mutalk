60a420211af3a44fb423fa79cd98ea6b837a0bc2
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2022-11-03 02:36:16 +0900

    implemented top app bar

--- app/src/main/java/io/mc/mutalk/MainActivity.java
+++ app/src/main/java/io/mc/mutalk/MainActivity.java
@@ -1,15 +1,17 @@
 package io.mc.mutalk;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,26 +20,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
+        FrameLayout contentFrame = findViewById(R.id.content_frame);
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnItemSelectedListener(item -> {
+        Fragment homeFragment = new HomeFragment();
+        Fragment wallsFragment = new WallsFragment();
+
+        // Top App Bar
+        topAppBar.setOnMenuItemClickListener((Toolbar.OnMenuItemClickListener) item -> {
             switch(item.getItemId()) {
-                case R.id.home:
-                    Toast.makeText(this, "홈", Toast.LENGTH_SHORT).show();
+                case R.id.search:
+                    //TODO: implement search function
+                    Toast.makeText(this, "Search function is not implemented.\nHere's ChatActivity for you instead.", Toast.LENGTH_SHORT).show();
+                    // show ChatActivity instead
+                    Intent chatIntent = new Intent(this, ChatActivity.class);
+                    this.startActivity(chatIntent);
                     return true;
-                case R.id.page2:
-                    Toast.makeText(this, "메뉴2", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.page3:
-                    Toast.makeText(this, "메뉴3", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.page4:
-                    Toast.makeText(this, "메뉴4", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.page5:
-                    Toast.makeText(this, "메뉴5", Toast.LENGTH_SHORT).show();
+                case R.id.account:
+                    //TODO: implment AccountActivity
+                    Toast.makeText(this, "AccountActivity is not implemented.\nHere's SigninActivity for you instead.", Toast.LENGTH_SHORT).show();
+                    // show SigninActivity instead
+                    Intent signinIntent = new Intent(this, SigninActivity.class);
+                    this.startActivity(signinIntent);
                     return true;
             }
             return false;
         });
+
+        // Bottom Navigation Bar
+        //TODO: do not switch fragment if already on current one
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.home:
+                    // open HomeFragment
+                    getSupportFragmentManager().beginTransaction().replace(contentFrame.getId(), homeFragment).commit();
+                    return true;
+                case R.id.walls:
+                    // open wallsFragment
+                    getSupportFragmentManager().beginTransaction().replace(contentFrame.getId(), wallsFragment).commit();
+                    return true;
+                case R.id.page3:
+                    //TODO: replace page3 to meaningful id name
+                    Toast.makeText(this, "Page3Fragment is not implemented.", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        });
+
+        // open HomeFragment by default
+        getSupportFragmentManager().beginTransaction().replace(contentFrame.getId(), homeFragment).commit();
     }
 }
--- app/src/main/java/io/mc/mutalk/SigninActivity.java
+++ app/src/main/java/io/mc/mutalk/SigninActivity.java
@@ -1,8 +1,11 @@
 package io.mc.mutalk;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        // add back button to top app bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // add functionality to back button in top app bar
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
